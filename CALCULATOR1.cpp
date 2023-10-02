5f3fee1a624c707008dda3b526ad3fab651e530c
RAGULRAJ12345 <146200452+RAGULRAJ12345@users.noreply.github.com>
2023-10-01 21:23:59 -0700

    CALCULATOR
    C LANGUAGE

---
+++ CALCULATOR1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+		int a,b,c;
+	a;
+	printf("enter the number\n");
+	scanf("%d",&a);
+	b;
+	printf("enter the another value\n ");
+	scanf("%d",&b);
+	c = a+b;
+	printf("the addition for the number you entered is %d\n\n",c);
+	c = a-b;
+	printf("the subraction for the number is%d\n\n\n",c);
+	c = a*b;
+	printf("the multiplication for the number you entered is%d\n\n\n",c);
+	c = a/b;
+	printf("the division for the number you entered is%d\n\n\n",c);
+	c =a%b;
+	printf("the moldule for the number you entered is%d\n\n\n",c);
+	
+	return 0;
+	
+	
+}
